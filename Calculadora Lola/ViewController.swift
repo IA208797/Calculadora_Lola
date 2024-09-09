c4335a9016fb6c0c0414716bc814973832ba53cd
Izant <83232702+IA208797@users.noreply.github.com>
2024-09-09 10:41:29 -0600

    [AGREGA] Máquina de estados y algunas funciones
    Se comienza a ver la funcionalidad de los botones con operaciones vinculadas.

--- Calculadora Lola/ViewController.swift
+++ Calculadora Lola/ViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
+enum estados_de_la_calculadora {
+    case seleccionar_numeros
+    case escoger_operacion
+    case mostrar_resultados
+}
+
 class ViewController: UIViewController {
+    var estado_actual: estados_de_la_calculadora = estados_de_la_calculadora.seleccionar_numeros
     
     @IBOutlet weak var texto_a_cambiar: UILabel!
     @IBOutlet weak var boton_operacion: UIButton!
     
     //Utilizamos un diccionario para guardar elementos
     var botones_interfaz: Dictionary<String, IUBotonCalculadora> = [:]
+    var operacion_actual: String? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,11 +34,31 @@ class ViewController: UIViewController {
     /// <#Description#>
     /// - Parameter sender: <#sender description#>
     @IBAction func que_hacer_al_pushar_boton(_ sender: UIButton) {
-        // TODO: Arreglar glitch del text quitando el optional
-        let text_a_añadir = botones_interfaz[(sender.restorationIdentifier ?? boton_operacion.restorationIdentifier) ?? "boton"]?.numero
-        texto_a_cambiar.text = "\(texto_a_cambiar.text ?? "")\(text_a_añadir!)"
-    }
+            if(estado_actual == estados_de_la_calculadora.seleccionar_numeros){
+                // TODO: Arreglar glitch del text quitando el optional
+                let text_a_añadir = botones_interfaz[(sender.restorationIdentifier ?? boton_operacion.restorationIdentifier) ?? "boton"]?.numero
+                
+                texto_a_cambiar.text = "\(texto_a_cambiar.text ?? "")\(text_a_añadir!)"
+            }
+            else if (estado_actual == estados_de_la_calculadora.escoger_operacion){
+                if let _mensajero: UIButton? = sender{
+                    operacion_actual = botones_interfaz[_mensajero!.restorationIdentifier ?? "boton_0"]?.operacion
+                }
+                else {
+                    operacion_actual = nil
+                }
+            }
+
+        }
     
+    @IBAction func boton_escoger_operacion_pulsado(_ sender: UIButton){
+        if (estado_actual == estados_de_la_calculadora.seleccionar_numeros){
+            estado_actual = estados_de_la_calculadora.escoger_operacion
+            dibujar_numeros_u_operaciones_en_interfaz()
+            
+
+        }
+    }
     
     func inicializar_calculadora() -> Void{
         crear_arreglo_botones()
@@ -40,7 +68,13 @@ class ViewController: UIViewController {
         botones_interfaz = IUBotonCalculadora("-", numero: Character("0"), operacion: "").crear_arreglo_botones()
     }
     
-    
+    func dibujar_numeros_u_operaciones_en_interfaz(){
+        if(estado_actual == estados_de_la_calculadora.escoger_operacion){
+            
+        }else if(estado_actual == estados_de_la_calculadora.seleccionar_numeros){
+            
+        }
+    }
 
 }
 
