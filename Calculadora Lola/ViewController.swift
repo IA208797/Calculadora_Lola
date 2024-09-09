ffba5262b8d32f1ae59f64bd6d3f66a046c8aa86
Izant <83232702+IA208797@users.noreply.github.com>
2024-09-09 10:20:28 -0600

    [CREA] Calculadora Lola

---
+++ Calculadora Lola/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Calculadora Lola
+//
+//  Created by Ismael DC on 08/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var texto_a_cambiar: UILabel!
+    @IBOutlet weak var boton_operacion: UIButton!
+    
+    //Utilizamos un diccionario para guardar elementos
+    var botones_interfaz: Dictionary<String, IUBotonCalculadora> = [:]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        inicializar_calculadora()
+        
+    }
+    
+    
+    /// <#Description#>
+    /// - Parameter sender: <#sender description#>
+    @IBAction func que_hacer_al_pushar_boton(_ sender: UIButton) {
+        // TODO: Arreglar glitch del text quitando el optional
+        let text_a_añadir = botones_interfaz[(sender.restorationIdentifier ?? boton_operacion.restorationIdentifier) ?? "boton"]?.numero
+        texto_a_cambiar.text = "\(texto_a_cambiar.text ?? "")\(text_a_añadir!)"
+    }
+    
+    
+    func inicializar_calculadora() -> Void{
+        crear_arreglo_botones()
+    }
+    
+    func crear_arreglo_botones() -> Void {
+        botones_interfaz = IUBotonCalculadora("-", numero: Character("0"), operacion: "").crear_arreglo_botones()
+    }
+    
+    
+
+}
+
