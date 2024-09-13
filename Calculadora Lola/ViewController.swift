688d85a8a4057268e7b233ce01f516011999e45c
Izant <83232702+IA208797@users.noreply.github.com>
2024-09-13 10:51:17 -0600

    [AGREGA] Últimos toques de funcionalidad
    Pero perdí las conexiones entre el viewController y el storyboard ☠️

--- Calculadora Lola/ViewController.swift
+++ Calculadora Lola/ViewController.swift
@@ -10,18 +10,24 @@ import UIKit
 enum estados_de_la_calculadora {
     case seleccionar_numeros
     case escoger_operacion
-    case mostrar_resultados
+    case mostrar_resultado
 }
 
 class ViewController: UIViewController {
-    var estado_actual: estados_de_la_calculadora = estados_de_la_calculadora.seleccionar_numeros
+    var estado_actual: estados_de_la_calculadora = .mostrar_resultado
     
+
+//Perdí las conexiones😵
     @IBOutlet weak var texto_a_cambiar: UILabel!
     @IBOutlet weak var boton_operacion: UIButton!
+    @IBOutlet weak var segundo_termino_texto: UILabel!
+    @IBOutlet weak var operacion_texto: UILabel!
+    @IBOutlet weak var vista_stack: UIStackView!
     
     //Utilizamos un diccionario para guardar elementos
     var botones_interfaz: Dictionary<String, IUBotonCalculadora> = [:]
     var operacion_actual: String? = nil
+    var numero_anterior: Double = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,47 +40,103 @@ class ViewController: UIViewController {
     /// <#Description#>
     /// - Parameter sender: <#sender description#>
     @IBAction func que_hacer_al_pushar_boton(_ sender: UIButton) {
-            if(estado_actual == estados_de_la_calculadora.seleccionar_numeros){
-                // TODO: Arreglar glitch del text quitando el optional
-                let text_a_añadir = botones_interfaz[(sender.restorationIdentifier ?? boton_operacion.restorationIdentifier) ?? "boton"]?.numero
-                
-                texto_a_cambiar.text = "\(texto_a_cambiar.text ?? "")\(text_a_añadir!)"
+        if(estado_actual == estados_de_la_calculadora.seleccionar_numeros){
+            // TODO: Arreglar glitch del text quitando el optional
+            if let _mensajero_id = sender.restorationIdentifier{
+                let texto_cache = botones_interfaz[_mensajero_id]?.numero
+                texto_a_cambiar.text = "\(texto_a_cambiar.text ?? "")\(texto_cache!)"
             }
-            else if (estado_actual == estados_de_la_calculadora.escoger_operacion){
-                if let _mensajero: UIButton? = sender{
-                    operacion_actual = botones_interfaz[_mensajero!.restorationIdentifier ?? "boton_0"]?.operacion
-                }
-                else {
-                    operacion_actual = nil
-                }
-            }
-
         }
+        else if (estado_actual == estados_de_la_calculadora.escoger_operacion){
+            if let _mensajero_id = sender.restorationIdentifier{
+                operacion_actual = botones_interfaz[_mensajero_id]?.operacion
+                estado_actual = estados_de_la_calculadora.seleccionar_numeros
+            }
+            else {
+                operacion_actual = nil
+            }
+        }
+        dibujar_numeros_u_operaciones_en_interfaz()
+    }
     
     @IBAction func boton_escoger_operacion_pulsado(_ sender: UIButton){
-        if (estado_actual == estados_de_la_calculadora.seleccionar_numeros){
-            estado_actual = estados_de_la_calculadora.escoger_operacion
-            dibujar_numeros_u_operaciones_en_interfaz()
-            
-
+        switch(estado_actual){
+        case .seleccionar_numeros:
+            estado_actual = .escoger_operacion
+        case .escoger_operacion:
+            estado_actual = .seleccionar_numeros
+        case .mostrar_resultado:
+            estado_actual = .escoger_operacion
         }
+        dibujar_numeros_u_operaciones_en_interfaz()
     }
     
     func inicializar_calculadora() -> Void{
         crear_arreglo_botones()
+        identificar_botones()
     }
     
     func crear_arreglo_botones() -> Void {
-        botones_interfaz = IUBotonCalculadora("-", numero: Character("0"), operacion: "").crear_arreglo_botones()
+        botones_interfaz = IUBotonCalculadora.crear_arreglo_botones()
     }
     
     func dibujar_numeros_u_operaciones_en_interfaz(){
-        if(estado_actual == estados_de_la_calculadora.escoger_operacion){
+        switch(estado_actual){
+        case .escoger_operacion:
+            for elemento in botones_interfaz.values{
+                elemento.referencia_a_boton_interfaz?.setTitle(
+                    elemento.operacion,
+                    for: .normal
+                )
+                elemento.referencia_a_boton_interfaz?.setTitle("Ñ", for: .selected)
+            }
             
-        }else if(estado_actual == estados_de_la_calculadora.seleccionar_numeros){
-            
+        case .seleccionar_numeros:
+            for elemento in botones_interfaz.values{
+                elemento.referencia_a_boton_interfaz?.setTitle(
+                    String(elemento.numero),
+                    for: .normal
+                )
+            }
+        case .mostrar_resultado:
+            0 == 0
         }
     }
-
+    
+    func identificar_botones(){
+        for pila_de_componentes in vista_stack.subviews{
+            for boton in pila_de_componentes.subviews{
+                if let identificador = boton.restorationIdentifier{
+                    botones_interfaz[identificador]?.referencia_a_boton_interfaz = boton as? UIButton
+                }
+            }
+        }
+    }
+    
+    @IBAction func obtener_resultado(_ sender: Any){
+        if numero_anterior != 0.0 && texto_a_cambiar.text != ""{
+            var numero_actual: Double = 0.0
+            if let numero_actual_string = texto_a_cambiar.text{
+                numero_actual = Double(numero_actual_string) ?? 0.0
+            }
+            
+            switch(operacion_actual){
+            case "+":
+                texto_a_cambiar.text = "\(numero_anterior + numero_actual)"
+            case "-":
+                texto_a_cambiar.text = "\(numero_anterior - numero_actual)"
+            case "*":
+                texto_a_cambiar.text = "\(numero_anterior * numero_actual)"
+            case "/":
+                texto_a_cambiar.text = "\(numero_anterior / numero_actual)"
+                
+            default:
+                texto_a_cambiar.text = "Hay un error"
+            }
+            estado_actual = estados_de_la_calculadora.mostrar_resultado
+        }
+    }
+    
 }
+
 
